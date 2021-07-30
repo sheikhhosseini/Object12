e22adfef6bf5b4b4fdc090059581b3f00b2cecd4
s.sheikh <mssheikh1377@gmail.com>
2021-07-30 15:56:10 +0430

    form group + validators Aded To Register Page

--- src/app/Pages/register/register.component.ts
+++ src/app/Pages/register/register.component.ts
@@ -23,32 +23,66 @@ export class RegisterComponent implements OnInit {
         null,
         [
           Validators.email,
+          Validators.maxLength(100),
           Validators.required
         ]
       ),
-      Password: new FormControl(),
-      ConfirmPassword: new FormControl(),
-      FirstName: new FormControl(),
-      LastName: new FormControl(),
-      MobileNumber: new FormControl(),
-      Gender: new FormControl(),
+      Password: new FormControl(null,
+        [
+          Validators.maxLength(15),
+          Validators.minLength(4),
+          Validators.required
+        ]),
+      ConfirmPassword: new FormControl(null,
+        [
+          Validators.maxLength(15),
+          Validators.minLength(4),
+          Validators.required
+        ]),
+      FirstName: new FormControl(null,
+        [
+          Validators.maxLength(100),
+          Validators.required
+        ]),
+      LastName: new FormControl(null,
+        [
+          Validators.maxLength(150),
+          Validators.required
+        ]),
+      MobileNumber: new FormControl(null,
+        [
+          Validators.maxLength(11),
+          Validators.minLength(11),
+          Validators.pattern("^[0-9]*$"),
+          Validators.required
+        ]),
+      Gender: new FormControl(null,
+        [
+          Validators.maxLength(15),
+          Validators.required
+        ]),
     });
   }
 
   SubmitRegisterForm() {
-    const registerData = new UserRegisterDto(
-      this.regitserForm.controls.Email.value,
-      this.regitserForm.controls.Password.value,
-      this.regitserForm.controls.ConfirmPassword.value,
-      this.regitserForm.controls.FirstName.value,
-      this.regitserForm.controls.LastName.value,
-      this.regitserForm.controls.MobileNumber.value,
-      this.regitserForm.controls.Gender.value);
 
-    console.log(registerData);
-    this._accountService.RegisterUser(registerData).subscribe(response =>{
-      console.log(response);
-    });
+    console.log(this.regitserForm);
+    // const registerData = new UserRegisterDto(
+    //   this.regitserForm.controls.Email.value,
+    //   this.regitserForm.controls.Password.value,
+    //   this.regitserForm.controls.ConfirmPassword.value,
+    //   this.regitserForm.controls.FirstName.value,
+    //   this.regitserForm.controls.LastName.value,
+    //   this.regitserForm.controls.MobileNumber.value,
+    //   this.regitserForm.controls.Gender.value);
+    //
+    // console.log(registerData);
+    // this._accountService.RegisterUser(registerData).subscribe(response =>{
+    //   console.log(response);
+    //   if (response.status === "Success") {
+    //     this.regitserForm.reset();
+    //   }
+    // });
 
   }
 }
